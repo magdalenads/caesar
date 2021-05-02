e31a16eebb4405ca809b0eb564c42c4ab9bd8c76
magdalena.dsr <magdalena@dueser.eu>
2021-05-02 12:53:55 +0200

    Formatted

--- Caesar.java
+++ Caesar.java
@@ -1,10 +1,8 @@
 /**
  * Die Caesar-Verschlüsselung!
  */
-public class Caesar
-{
-    public Caesar()
-    {
+public class Caesar {
+    public Caesar() {
         String text = "Reglvmglx er Gmgivs! Mgl, iyiv sfivwxiv Fijilpwlefiv, lefi iygl rmglx zivkiwwir yrh aimß zsr iyviv Peki. Lepxix rsgl imri aimpi wxerh yrh zivxveyx eyj iyvi Wxeivoi. Wglsr feph aivhir aimxivi Xvyttir dy iygl wxsßir, daim Pikmsrir wmrh fivimxw eyj hiq Aik. Kemyw Nypmyw Geiwev";
         char[] geheimtext = text.toCharArray();
         haeufigkeitsanalyse(geheimtext);
@@ -12,17 +10,17 @@ public class Caesar
 
     public int[] haeufigkeitsanalyse(char[] geheimtext) {
         int[] buchstabenzaehler = new int[128];
-        for(int i=0; i < geheimtext.length; i++){
-            int asciicode = (int)geheimtext[i];
+        for (int i = 0; i < geheimtext.length; i++) {
+            int asciicode = (int) geheimtext[i];
             buchstabenzaehler[asciicode]++;
         }
         return buchstabenzaehler;
     }
 
     public void analyseAusgabe(int[] buchstabenzaehler) {
-        for (int ascii=0; ascii < buchstabenzaehler.length; ascii++) {
-            char buchstabe = (char)ascii;
-            System.out.println("Anzahl von " + buchstabe + " ist " + buchstabenzaehler[ascii]); 
+        for (int ascii = 0; ascii < buchstabenzaehler.length; ascii++) {
+            char buchstabe = (char) ascii;
+            System.out.println("Anzahl von " + buchstabe + " ist " + buchstabenzaehler[ascii]);
         }
     }
 
@@ -33,10 +31,10 @@ public class Caesar
     }
     } */
 
-    public char[] verschluesseln(char[] klartext, int schluessel){
+    public char[] verschluesseln(char[] klartext, int schluessel) {
         char[] geheimtext = new char[klartext.length];
 
-        for(int i = 0; i < klartext.length; i++){
+        for (int i = 0; i < klartext.length; i++) {
             int verschiebung = klartext[i] + schluessel;
             if (verschiebung >= 128) {
                 verschiebung -= 128;
@@ -47,10 +45,10 @@ public class Caesar
         return geheimtext;
     }
 
-    public char[] entschluesseln(char[] geheimtext, int schluessel){
+    public char[] entschluesseln(char[] geheimtext, int schluessel) {
         char[] klartext = new char[geheimtext.length];
 
-        for(int i = 0; i < geheimtext.length; i++){
+        for (int i = 0; i < geheimtext.length; i++) {
             int verschiebung = geheimtext[i] - schluessel;
             if (verschiebung < 0) {
                 verschiebung += 128;
